2f4485d9c0b5acf8321254c13bb7e91d6f82569d
wanglili <lili.a.wang@intel.com>
2012-10-20 21:15:44 +0800

    Initialize VppSettings code.[MRFLD-VIDEO]
    BZ: 63471
    Submit the initial version of VppSettings, the java application to
    configure Video Post-Processing function on or off.
    
    Change-Id: Id3a251bafdf43dea61c3e3eb42f96c935a26f734
    Reviewed-on: http://android.intel.com:8080/70970
    Reviewed-by: Wang, Lili A <lili.a.wang@intel.com>
    Reviewed-by: Shi, PingX <pingx.shi@intel.com>
    Tested-by: Shi, PingX <pingx.shi@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

---
+++ src/com/intel/vpp/VppSettings.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.vpp;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class VppSettings extends Activity implements CompoundButton.OnCheckedChangeListener {
+
+    private static final String TAG = "VppSettings";
+
+    private static final String VPP_SHARED_PREF = "vpp_settings";
+    private static final String VPP_STATUS = "vpp_status";
+
+    private boolean mStatus;
+    private SharedPreferences mSharedPref;
+    private Switch mSwitch;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        View customBar = getLayoutInflater().inflate(R.layout.custom_bar, null);
+        mSwitch = (Switch)customBar.findViewById(R.id.switchWidget);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setCustomView(customBar);
+        getActionBar().setDisplayShowCustomEnabled(true);
+
+        setContentView(R.layout.act_settings);
+
+        mSharedPref = getSharedPreferences(VPP_SHARED_PREF, 0);
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mStatus = mSharedPref.getBoolean(VPP_STATUS, false);
+        mSwitch.setOnCheckedChangeListener(this);
+        mSwitch.setChecked(mStatus);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mSwitch.setOnCheckedChangeListener(null);
+    }
+
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        mStatus = isChecked;
+        SharedPreferences.Editor editor = mSharedPref.edit();
+        editor.putBoolean(VPP_STATUS, mStatus).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return true;
+    }
+}
