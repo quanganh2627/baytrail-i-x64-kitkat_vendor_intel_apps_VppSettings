0280d5d8e2a948d46f4f9797c5ad79860f17c345
Yu, Linda <linda.yu@intel.com>
2013-07-23 12:11:44 +0800

    expose vpp strength settings UI to user
    BZ: 124971
    
    user is able to selete low/medium/high vpp strength from vpp settings
    
    Change-Id: I18f5b7fdb92d4b0f11822179ae072f6966b8a164
    Signed-off-by: Yu, Linda <linda.yu@intel.com>
    Reviewed-on: http://android.intel.com:8080/119873
    Reviewed-by: Wang, Lili A <lili.a.wang@intel.com>
    Reviewed-by: Shi, PingX <pingx.shi@intel.com>
    Tested-by: Shi, PingX <pingx.shi@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- src/com/intel/vpp/VppSettings.java
+++ src/com/intel/vpp/VppSettings.java
@@ -17,37 +17,51 @@
 package com.intel.vpp;
 
 import android.app.Activity;
+
 import android.content.SharedPreferences;
+
 import android.os.Bundle;
+
 import android.util.Log;
+
 import android.view.MenuItem;
 import android.view.View;
+
+import android.widget.AdapterView;
 import android.widget.CompoundButton;
+import android.widget.Spinner;
 import android.widget.Switch;
 
-public class VppSettings extends Activity implements CompoundButton.OnCheckedChangeListener {
+public class VppSettings extends Activity implements CompoundButton.OnCheckedChangeListener,
+     AdapterView.OnItemSelectedListener {
 
     private static final String TAG = "VppSettings";
 
     private static final String VPP_SHARED_PREF = "vpp_settings";
     private static final String VPP_STATUS = "vpp_status";
+    private static final String VPP_STRENGTH = "strength";
+    private static final String[] VPP_STRENGTH_VALUES = {"0low", "1medium", "2high"};
 
     private boolean mStatus;
+    private int mStrength;
     private SharedPreferences mSharedPref;
     private Switch mSwitch;
+    private View box;
+    private View divider;
+    private Spinner spinner;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
-        View customBar = getLayoutInflater().inflate(R.layout.custom_bar, null);
-        mSwitch = (Switch)customBar.findViewById(R.id.switchWidget);
-
         getActionBar().setDisplayHomeAsUpEnabled(true);
-        getActionBar().setCustomView(customBar);
-        getActionBar().setDisplayShowCustomEnabled(true);
 
         setContentView(R.layout.act_settings);
+
+        mSwitch = (Switch)findViewById(R.id.switcher);
+        box = (View)findViewById(R.id.box);
+        divider = (View)findViewById(R.id.divider);
+        spinner = (Spinner)findViewById(R.id.spinner);
 
         mSharedPref = getSharedPreferences(VPP_SHARED_PREF, Activity.MODE_WORLD_READABLE);
     }
@@ -57,8 +71,14 @@ public class VppSettings extends Activity implements CompoundButton.OnCheckedCha
     public void onResume() {
         super.onResume();
         mStatus = mSharedPref.getBoolean(VPP_STATUS, false);
+        mStrength = Integer.valueOf(mSharedPref.getString(VPP_STRENGTH, "1medium").substring(0, 1));
+        SharedPreferences.Editor editor = mSharedPref.edit();
+        editor.putString(VPP_STRENGTH, VPP_STRENGTH_VALUES[mStrength]).commit();
         mSwitch.setOnCheckedChangeListener(this);
         mSwitch.setChecked(mStatus);
+        checkUI();
+        spinner.setSelection(mStrength);
+        spinner.setOnItemSelectedListener(this);
     }
 
     @Override
@@ -67,10 +87,32 @@ public class VppSettings extends Activity implements CompoundButton.OnCheckedCha
         mSwitch.setOnCheckedChangeListener(null);
     }
 
+    private void checkUI() {
+        if (mStatus) {
+            box.setVisibility(View.VISIBLE);
+            divider.setVisibility(View.VISIBLE);
+        } else {
+            box.setVisibility(View.GONE);
+            divider.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        mStrength = position;
+        SharedPreferences.Editor editor = mSharedPref.edit();
+        editor.putString(VPP_STRENGTH, VPP_STRENGTH_VALUES[mStrength]).commit();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+    }
+
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         mStatus = isChecked;
         SharedPreferences.Editor editor = mSharedPref.edit();
         editor.putBoolean(VPP_STATUS, mStatus).commit();
+        checkUI();
     }
 
     @Override
